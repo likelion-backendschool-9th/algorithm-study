36950924c44f0b39b4561c13e68e9430def105fd
Jihye Moon <jihyer0395@gmail.com>
2026-09-08 14:20:20 +0900

    programmers / 42885_구명보트 / 지혜

--- programmers/42885_구명보트/jihye.java
+++ programmers/42885_구명보트/jihye.java
@@ -1,9 +1,25 @@
-class Solution
-{
-    public int solution()
-    {
+import java.util.*;
+
+//시작 시간: 26.09.08 오후 2시 11분
+//종료 시간: 26.09.08 오후 
+class Solution {
+    
+    public int solution(int[] people, int limit) {
+        int start = 0, end = people.length-1;
         int answer = 0;
+        
+        Arrays.sort(people);
+        
+        while(start <= end){
+            if(people[start]+people[end] <= limit){
+                start++;
+            }
+            
+            end--;
+            answer++;
+        }
         
         return answer;
     }
+    
 }
