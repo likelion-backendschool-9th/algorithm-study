eb917afde3f651ccd81c22695b43777306b110ff
양덕관 <ydk9819@gmail.com>
2026-09-10 14:12:01 +0900

    힌트스테이지_덕관 간지나는 주석 삽입(?)

--- programmers/468377_힌트스테이지/deokkwan.py
+++ programmers/468377_힌트스테이지/deokkwan.py
@@ -1,26 +1,26 @@
 def solution(cost, hint):
-    answer = [float('inf')]
-    n = len(cost)
+    answer = [float('inf')] # 큰 값 대입
+    n = len(cost) # cost 배열의 길이로 총 스테이지 수를 구함
     
+    # 재귀 함수를 통한 완전 탐색
     def dfs(i, idx_arr, total):
-        if i == n - 1:
+        if i == n - 1: # 재귀 함수를 멈추는 조건문
             answer[0] = min(answer[0], total + cost[i][idx_arr[i]])
             return
         
-        total += cost[i][idx_arr[i]]
-        dfs(i + 1, idx_arr, total)
+        total += cost[i][idx_arr[i]] # 현재 단계 cost를 total에 합
+        dfs(i + 1, idx_arr, total) # 힌트 번들을 안샀을 때
         
-        new_idx_arr = idx_arr[:]
+    
+        new_idx_arr = idx_arr[:] # 다음 재귀를 위해 복사
         for j in hint[i][1:]:
-            if new_idx_arr[j - 1] == n - 1:
+            if new_idx_arr[j - 1] == n - 1: # cost 배열 마지막 index를 가리키는 경우
                 continue
             
-            new_idx_arr[j - 1] += 1
+            new_idx_arr[j - 1] += 1 
         
-        
-        dfs(i + 1, new_idx_arr, total + hint[i][0])
+        dfs(i + 1, new_idx_arr, total + hint[i][0]) # 힌트 번들을 샀을때
     
-    
-    dfs(0, [0] * n, 0)    
+    dfs(0, [0] * n, 0)
     
     return answer[0]
